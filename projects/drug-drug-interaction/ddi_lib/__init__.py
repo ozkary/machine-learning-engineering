7c0b974462e02c4cc3f9e724c1878a79136fa54e
ozkary <ozkary@ozkary-dev-2020>
2023-12-16 20:04:32 -0500

    remove the large gz file

--- projects/drug-drug-interaction/ddi_lib/__init__.py
+++ projects/drug-drug-interaction/ddi_lib/__init__.py
@@ -3,5 +3,4 @@ from .__version__ import __version__
 from .data_train import DDIProcessData, DDIModelFactory
 from .data_train_mlp import DDIMLPFactory, DDIProcessor
 
-
 print(f"Initializing drug-drug interaction ddi_lib {__version__}")
